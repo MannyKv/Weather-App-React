2262f761f54f8326bd533b685b829dca5af11871
Mlal371 <mlal371@aucklanduni.ac.nz>
2023-02-19 13:49:21 +1300

    feat: API and hooks installed and basic functionality created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
 import './App.css';
+import Weather from './components/Weather';
 
-function App() {
+ function App() {
 
+    const [lat, setLat] = useState([]);
+    const [long, setLong] = useState([]);
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                setLat(position.coords.latitude);
+                setLong(position.coords.longitude);
+            });
+            
+            await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+                .then(res => res.json())
+                .then(result => {
+                    setData(result)
+                    console.log(result);
+                });
+        }
+        fetchData();
+    }, [lat,long])
+
+    return (
+        <div className={"App"}>
+
+            {(typeof data.main != 'undefined') ? ( // if data is undefined return empty div
+                <Weather weatherData={data}/>
+            ) : (
+                <div></div>
+            )}
+
+
+        </div>
+
+    )
 }
 
 export default App;
